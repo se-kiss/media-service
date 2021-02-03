368dc9cecf73a7987367461ad4ee040129824d7e
NestZ <NestZ671@gmail.com>
2021-02-03 23:52:19 +0700

    init elasticsearch module

--- src/media/media.module.ts
+++ src/media/media.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
+import { ElasticsearchModule } from '@nestjs/elasticsearch';
 import { MediaController } from './media.controller';
 import { MediaService } from './media.service';
 
 @Module({
+  imports: [
+    ElasticsearchModule.registerAsync({
+      useFactory: async () => ({
+        node: process.env.ELASTICSEARCH_NODE,
+        auth: {
+          username: process.env.ELASTICSEARCH_USERNAME,
+          password: process.env.ELASTICSEARCH_PASSWORD,
+        },
+      }),
+    }),
+  ],
   controllers: [MediaController],
-  providers: [MediaService]
+  providers: [MediaService],
+  exports: [ElasticsearchModule],
 })
 export class MediaModule {}
